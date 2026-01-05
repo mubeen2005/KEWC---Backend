b4636a887e1581e459355cbc5dc5c8d9c76308bf
Mubeen Shaikh <mubinshaikj666@gmail.com>
2026-01-05 09:55:34 +0530

    email update

--- controller/sendEmail.js
+++ controller/sendEmail.js
@@ -4,16 +4,17 @@ require("dotenv").config();
 // transporter (one time)
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // 465 ke liye true zaroori hai
+  port: 587,
+  secure: false, // 587 ke liye false hona chahiye
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  // Timeout badhane ke liye ye options add karein
-  connectionTimeout: 10000, 
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  tls: {
+    // Ye line important hai agar certificate issue ho raha ho
+    rejectUnauthorized: false 
+  },
+  connectionTimeout: 15000, 
 });
 
 // reusable function
